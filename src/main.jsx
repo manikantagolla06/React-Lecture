14ad26c225a93eb071b0ee4035fd4ff7f2fcaf8f
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-23 20:23:00 +0530

    sds

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,8 @@ import "./index.css";
 import App from "./App.jsx";
 import Test from "./components/Test.jsx";
 createRoot(document.getElementById("root")).render(<App />);
+
+//React creates virtual DOM
+//Physical root
+//Virtual root
+//Reactjs updates virtual root, which automatically updated physical root
