f617efcd0fb6055127610c16e02a3d2c590c36f7
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-24 19:41:02 +0530

    dc

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from "/vite.svg";
 import App1 from "./components/App1";
 import App2 from "./components/App2";
 import App3 from "./components/App3";
+import App4 from "./components/App4";
+import App5 from "./components/App5";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -17,6 +19,8 @@ function App() {
         <Route path="/" element={<App1 />}></Route>
         <Route path="/app2" element={<App2 />}></Route>
         <Route path="/app3" element={<App3 />}></Route>
+        <Route path="/app4" element={<App4 />}></Route>
+         <Route path="/app5" element={<App5 />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
