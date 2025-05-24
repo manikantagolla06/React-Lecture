aaa81df01032ad9414e4d8645dfc9f95dd4d2a0f
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-24 20:13:43 +0530

    dsc

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route index element={<App1 />}></Route>
         <Route path="/" element={<App1 />}></Route>
-        <Route path="/app2" element={<App2 />}></Route>
+        
         <Route path="/app3" element={<App3 />}></Route>
         <Route path="/app4" element={<App4 />}></Route>
          <Route path="/app5" element={<App5 />}></Route>
