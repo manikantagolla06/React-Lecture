1effffa3c7cdf15597e0446c298aa9cc5b26934d
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-25 20:07:08 +0530

    effef

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-
 export default function Product({ product, onAdd }) {
+  if (!product) {
+    return <div>No product data available.</div>;
+  }
+
   return (
     <div className="App-Product-Box">
       <h2>{product.name}</h2>
