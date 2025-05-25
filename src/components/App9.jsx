3fcb44610d6c5b15d8520f794818a0f24f0c6dc1
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-25 17:41:44 +0530

    fsw

---
+++ src/components/App9.jsx
@@ -0,0 +1,27 @@
+
+import React from "react";
+import { useState } from "react";
+export default function App9() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    //setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
+  };
+  const decrement = () => {
+    //setCount(count + 1);
+    setCount((prevCount) => prevCount - 1);
+  };
+  return (
+    <div>
+      <h1>App8</h1>
+      <h2>useState Revision</h2>
+      <h3>{count}</h3>
+      <p>
+        <button onClick={decrement}>-</button>
+         <h3>{count}</h3>
+        <button onClick={decrement}>+</button>
+        
+      </p>
+    </div>
+  );
+}
