93030e25470993c8a857579d157985ce45eacb3f
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-05-26 19:20:36 +0530

    wfdw

--- src/components/App14.jsx
+++ src/components/App14.jsx
@@ -1,31 +1,38 @@
-import React from "react";
-import { useState, useEffect } from "react";
 import axios from "axios";
-export default function App14() {
-  const [users, setUsers] = useState([]);
-  const fetchData = async () => {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    const res = await axios(url);
-    setUsers(res.data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <div>
-      <h1>App14</h1>
-      <h2>useEffect hook to fetch api</h2>
-      <p>
-        <input type="text" placeholder="Search name" />
-      </p>
-      <ol>
-        {users &&
-          users.map((value) => (
-            <li key={value.id}>
-              {value.name}, {value.email}, {value.phone}
-            </li>
-          ))}
-      </ol>
-    </div>
-  );
+import React, { useEffect, useState } from "react";
+function App14(){
+    const [users,setUsers]=useState([])
+    const [search,setSearch]=useState("")
+    const fetchData=async()=>{
+        const url="https://jsonplaceholder.typicode.com/users";
+        const res=await axios.get(url);
+        setUsers(res.data);
+    }  
+
+    useEffect(()=>{
+       fetchData(); 
+    },[])
+
+    const names=users.filter((user)=>
+        user.name.toLowerCase().includes(search.toLowerCase())
+    );
+    
+    return(
+        <div>
+            <h1>App14</h1>
+            <h2>useEffect hook to fetch api</h2>
+            <p>
+                <input type="text" placeholder="search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+
+            </p>
+            <ul>
+                {names.map((user) => (
+                    <li key={user.id}>
+                        <strong>ID:</strong>{user.id} |<strong>Name:</strong>{user.name} |<strong>City:</strong>{user.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
+export default App14;
